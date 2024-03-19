11ee9db4c0b90a8a23c09b8e175d14e32977b097
ManasRanjanBariha <manasbariha779@gmail.com>
2024-03-20 00:56:21 +0530

    updating the Product Component

--- WECARE/src/components/Home/ProductCard.jsx
+++ WECARE/src/components/Home/ProductCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { AiOutlineStar } from "react-icons/ai";
+import { BsStar } from "react-icons/bs";
 const ProductCard = () => {
   return (
     <div className="card rounded shadow-sm border-0 my-2 cursor-pointer">
@@ -7,8 +8,8 @@ const ProductCard = () => {
       <h5  className="text-dark">Awesome product</h5>
       <h5></h5>
       <h6 className='small'>MRP <span className='text-decoration-line-through text-muted'>₹ 100</span> <span className='text-success fw-bold'>10% OFF</span></h6>
-      <h5 className='fs-4 fw-bold'>₹ 90</h5>
-      <button className='btn btn-primary w-100 fw-bold '>Add to cart</button>
+      <h5 className='fs-4 fw-bold d-flex justify-content-between'><span>₹ 90</span> <span className='bg-success  fw-lighter px-2 text-center pb-1 rounded-2 d-flex align-items-center fs-6 text-white'>4.5 <span><AiOutlineStar/></span> </span></h5>
+      <button className='btn w-100 fw-bold text-white btn-red ' style={{background:"#ff6f6f"}}>Add to cart</button>
     </div>
   </div>
   )
